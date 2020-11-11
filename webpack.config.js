4e7999ae72cc3a8e9bb0483927df502ff9f95b15
raycho206 <careful1617@gmail.com>
2020-11-11 15:46:17 +0900

    webpack-cli 4.x.x버전에서 webpack-dev-server hot reload 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
   name: 'react-toy',
@@ -27,13 +28,22 @@ module.exports = {
           ],
           plugins: [
             '@babel/plugin-proposal-class-properties',
+            'react-refresh/babel',
           ],
         },
       },
     ],
   },
+  plugins: [
+    new RefreshWebpackPlugin(),
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.js',
+    publicPath: '/dist/',
   }, // 출력
+  devServer: {
+    publicPath: '/dist/',
+    hot: true,
+  },
 };
