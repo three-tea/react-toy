f95b6fb6f2e3da20704714286160c673841d5492
raycho206 <careful1617@gmail.com>
2020-11-14 22:46:33 +0900

    redux-actions 추가

--- src/store/modules/counter.js
+++ src/store/modules/counter.js
@@ -1,38 +1,23 @@
+import { createAction, handleActions } from 'redux-actions';
+
 // 액션 타입 정의
 const CHANGE_COLOR = 'counter/CHANGE_COLOR';
-const INCREMENT = 'counter/INCREMENT';
-const DECREMENT = 'counter/DECREMENT';
 
 // 액션 생섬함수 정의
-export const changeColor = color => ({ type: CHANGE_COLOR, color });
-export const increment = () => ({ type: INCREMENT });
-export const decrement = () => ({ type: DECREMENT });
+export const changeColor = createAction(CHANGE_COLOR, text => text);
 
 // **** 초기상태 정의
 const initialState = {
   color: 'red',
-  number: 0,
 };
 
 // **** 리듀서 작성
-export default function counter(state = initialState, action) {
-  switch (action.type) {
-    case CHANGE_COLOR:
-      return {
-        ...state,
-        color: action.color,
-      };
-    case INCREMENT:
-      return {
-        ...state,
-        number: state.number + 1,
-      };
-    case DECREMENT:
-      return {
-        ...state,
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+export default handleActions(
+  {
+    [CHANGE_COLOR]: (state, action) => ({
+      ...state,
+      color: action.payload,
+    }),
+  },
+  initialState,
+);
